5c18215f4818a98f49abf0a6eb82e3cfc67e58b6
root <root@m15364.contaboserver.net>
2024-04-16 01:55:58 -0500

    google-flan-t5-small inference success

---
+++ main.py
@@ -0,0 +1,11 @@
+# pip install accelerate
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-small", device_map="auto")
+
+input_text = "What is your name?"
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
+
+outputs = model.generate(input_ids)
+print(tokenizer.decode(outputs[0]))
