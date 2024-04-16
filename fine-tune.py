26a05559db08371a5cc1a3bc4d2dca8c4e550c3b
mltrev23 <trevor.dev23@gmail.com>
2024-04-16 14:10:32 -0500

    final round

--- fine-tune.py
+++ fine-tune.py
@@ -157,24 +157,3 @@ trainer.fit(model)
 
 save_directory = "." # save in the current working directory, you can change this of course
 model.model.save_pretrained(save_directory)
-
-from datasets import load_dataset
-
-dataset = load_dataset("code_x_glue_ct_code_to_text", "ruby")
-print(dataset['test'])
-
-test_example = dataset['test'][2]
-print("Code:", test_example['code'])
-
-from transformers import T5ForConditionalGeneration
-
-model = T5ForConditionalGeneration.from_pretrained(save_directory)
-
-# prepare for the model
-input_ids = tokenizer(test_example['code'], return_tensors='pt').input_ids
-# generate
-outputs = model.generate(input_ids)
-print("Generated docstring:", tokenizer.decode(outputs[0], skip_special_tokens=True))
-
-print("Ground truth:", test_example['docstring'])
-"""
